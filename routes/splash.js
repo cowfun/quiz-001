7aed506e4b26ddebe1f249946fe5e580824f7435
Calvin Hui <cowfun87@gmail.com>
2017-10-10 13:24:53 -0700

    Finished the timestamp element by including a new plugin called moment.js

--- routes/splash.js
+++ routes/splash.js
@@ -4,13 +4,16 @@ const multer = require('multer');
 const path = require('path');
 const kx = require('../db/connection');
 
+const moment = require('moment');
+moment().format();
+
 router.get('/', function (request, response) {
   kx
     .select()
     .from("clucks")
     .orderBy('created_at', 'DESC')
     .then((clucks)=>{
-      response.render('index',{clucks})
+      response.render('index',{clucks, moment: moment})
     });
 });
 
