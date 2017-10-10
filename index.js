4a1c98f1abd3cccb5f278ab4b6e5cc2676d51d40
Calvin Hui <cowfun87@gmail.com>
2017-10-10 09:19:01 -0700

    Setup Express skeleton, and installed the proper middleware in order to get the app to work correctly.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/test', (request, response) => {
+  response.send(`Welcome to Quiz, this is a response to see if server is working correctly`)
+})
+
+app.listen(5000);
+console.log('Server is listening on http://localhost:5000');
